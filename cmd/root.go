7eb7fe1b1353dfff671e75468acf818a2a69411b
Greg Oliver <golive@microsoft.com>
2021-06-03 11:20:36 +0100

    pacify linter

--- cmd/root.go
+++ cmd/root.go
@@ -78,6 +78,6 @@ func initConfig() {
 		_, err := os.Create(fileName)
 		cobra.CheckErr(err)
 		console.Warningf("Config file not found, creating new file %s with defaults\n", fileName)
-		viper.WriteConfig()
+		_ = viper.WriteConfig()
 	}
 }

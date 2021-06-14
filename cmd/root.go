a926a9048d82432b63adcd42e1a4a12a855200fe
Greg Oliver <divingclimber@gmail.com>
2021-06-14 18:15:39 +0100

    Add unit tests (#85)
    * add mock action, add testdata, add unit test
    * empty out a couple of the test files
    
    Co-authored-by: Greg Oliver <golive@microsoft.com>

--- cmd/root.go
+++ cmd/root.go
@@ -22,7 +22,9 @@ import (
 )
 
 var cfgFile string
-var actionMap = map[string]landingzone.Action{
+
+// ActionMap is exported so tests can use
+var ActionMap = map[string]landingzone.Action{
 	"init":     landingzone.NewInitAction(),
 	"plan":     landingzone.NewPlanAction(),
 	"apply":    landingzone.NewApplyAction(),
@@ -68,11 +70,11 @@ func init() {
 		os.Exit(1)
 	}
 	for _, ca := range custActions {
-		actionMap[ca.GetName()] = ca
+		ActionMap[ca.GetName()] = ca
 	}
 
 	// Dynamically build sub-commands from list of actions
-	for name, action := range actionMap {
+	for name, action := range ActionMap {
 		actionSubCmd := &cobra.Command{
 			Use:   name,
 			Short: action.GetDescription(),
@@ -81,7 +83,7 @@ func init() {
 			Run: func(cmd *cobra.Command, args []string) {
 				// NOTE: We CAN NOT use the action variable from the loop above as it's not bound at runtime
 				// Dynamically building our commands has some limitations, instead we need to use the cmd name & the map
-				action = actionMap[cmd.Name()]
+				action = ActionMap[cmd.Name()]
 
 				configFile, _ := cmd.Flags().GetString("config-file")
 				configPath, _ := cmd.Flags().GetString("config-dir")

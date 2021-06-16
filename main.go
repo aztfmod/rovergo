ff16005fcd07ab2d41c98b571c3a464b2ce8da4d
PaulDFoster <paulfo@microsoft.com>
2021-06-16 09:55:55 +0100

    Add startup checks for `az` and `terraform` Fixes #82 - PR requested changes done

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 //
 // Rover - main entry point
-// * Checks for dependent executables then
 // * Simply enters into the cobra root command and passes version
 // * Ben C, May 2021
 //
@@ -9,10 +8,8 @@ package main
 
 import (
 	"github.com/aztfmod/rover/cmd"
-	"github.com/aztfmod/rover/pkg/command"
 )
 
 func main() {
-	command.ValidateDependencies()
 	cmd.Execute()
 }

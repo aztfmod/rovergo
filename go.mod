1d832a027c7837c7d259e6a7ba3b364233c71a83
Ben Coleman <benc.uk@gmail.com>
2021-06-21 14:23:47 +0100

    Azure SDK changes require tenantID CheckIsOwner

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/aztfmod/rover
 go 1.16
 
 require (
-	github.com/Azure/azure-sdk-for-go v51.2.0+incompatible
+	github.com/Azure/azure-sdk-for-go v55.3.0+incompatible
 	github.com/Azure/azure-storage-blob-go v0.13.0
 	github.com/Azure/go-autorest/autorest v0.11.18
 	github.com/Azure/go-autorest/autorest/azure/auth v0.5.7
@@ -11,6 +11,7 @@ require (
 	github.com/Azure/go-autorest/autorest/to v0.4.0 // indirect
 	github.com/Azure/go-autorest/autorest/validation v0.3.1 // indirect
 	github.com/briandowns/spinner v1.13.0
+	github.com/davecgh/go-spew v1.1.1
 	github.com/hashicorp/go-version v1.3.0
 	github.com/hashicorp/terraform-exec v0.13.3
 	github.com/spf13/cobra v1.1.3
--- pkg/azure/auth_test.go
+++ pkg/azure/auth_test.go
@@ -14,16 +14,17 @@ func Test_IsOwnerCLI(t *testing.T) {
 	i := GetSignedInIdentity()
 	s := GetSubscription()
 
-	isOwner, err := CheckIsOwner(i.ObjectID, s.ID)
+	isOwner, err := CheckIsOwner(i.ObjectID, s.ID, s.TenantID)
 	assert.Nil(t, err)
 	assert.True(t, isOwner)
 }
 
 func Test_IsNotOwnerSub(t *testing.T) {
 	i := GetSignedInIdentity()
+	s := GetSubscription()
 
 	// Random GUID for subscription
-	isOwner, err := CheckIsOwner(i.ObjectID, "63f3ec63-61c7-432c-9a10-9513ec3f889e")
+	isOwner, err := CheckIsOwner(i.ObjectID, "63f3ec63-61c7-432c-9a10-9513ec3f889e", s.TenantID)
 	// This will error with 404
 	assert.NotNil(t, err)
 	detailedErr := err.(autorest.DetailedError)
@@ -35,7 +36,7 @@ func Test_IsNotOwnerOID(t *testing.T) {
 	s := GetSubscription()
 
 	// Random GUID for object id
-	isOwner, err := CheckIsOwner("5e441a4a-1da9-4f8e-8022-bd9debec9cc3", s.ID)
+	isOwner, err := CheckIsOwner("5e441a4a-1da9-4f8e-8022-bd9debec9cc3", s.ID, s.TenantID)
 	assert.Nil(t, err)
 	assert.False(t, isOwner)
 }

9f42d38db0637ee09e5376e32bdc038a67e7c007
PaulDFoster <paulfo@microsoft.com>
2021-06-23 16:58:42 +0100

    logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -4,61 +4,139 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/aztfmod/rover/pkg/command"
+	"github.com/aztfmod/rover/pkg/console"
 	"github.com/aztfmod/rover/pkg/utils"
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
-var LogEnvironment = "production"
-var logLevel = log.InfoLevel
+var stdOutLog = logrus.New()
+var stdOutEntry *logrus.Entry
+var fileLog = logrus.New()
+var logLevel = logrus.InfoLevel
+var currentFile string
+
+type Fields = map[string]interface{}
+
+// Only supporting fields on file logging
+func WithFields(fields Fields) *logrus.Entry {
+	stdOutEntry = stdOutLog.WithFields(fields)
+	console.Infof("Level: %d", stdOutEntry.Level)
+	return fileLog.WithFields(fields)
+}
+
+func Trace(args ...interface{}) {
+	stdOutLog.Trace(args...)
+	fileLog.Trace(args...)
+}
+
+func Debug(args ...interface{}) {
+	stdOutLog.Debug(args...)
+	fileLog.Debug(args...)
+}
+
+func Print(args ...interface{}) {
+	stdOutLog.Print(args...)
+	fileLog.Print(args...)
+}
+
+func Info(args ...interface{}) {
+	if stdOutEntry == nil {
+		console.Info("stdOutEntry is nil")
+		stdOutLog.Info(args...)
+	} else {
+		console.Info("stdOutEntry is not nil")
+		stdOutEntry.Info(args...)
+	}
+	fileLog.Info(args...)
+}
+
+func Warn(args ...interface{}) {
+	stdOutLog.Warn(args...)
+	fileLog.Warn(args...)
+}
+
+func Warning(args ...interface{}) {
+	stdOutLog.Warning(args...)
+	fileLog.Warning(args...)
+}
+
+func Error(args ...interface{}) {
+	stdOutLog.WithFields(logrus.Fields{"For details see": currentFile}).Error(args...)
+	fileLog.Error(args...)
+}
+
+func Panic(args ...interface{}) {
+	stdOutLog.WithFields(logrus.Fields{"For details see": currentFile}).Panic(args...)
+	fileLog.Panic(args...)
+}
+
+func Fatal(args ...interface{}) {
+	//stdOutLog.Fatal(args...) - fatal calls Exit(1)
+	stdOutLog.WithFields(logrus.Fields{"Fatal error encounterewd. For details see": currentFile}).Error(args...)
+	fileLog.Fatal(args...) // So logging to file only
+}
 
 func init() {
-	log.SetFormatter(&log.TextFormatter{
+	stdOutLog.SetFormatter(&logrus.TextFormatter{
 		ForceColors:      true,
 		DisableColors:    false,
 		FullTimestamp:    false,
 		DisableTimestamp: true,
 	})
+	stdOutLog.SetLevel(logrus.InfoLevel)
+	stdOutLog.SetOutput(os.Stdout)
 
-	log.SetLevel(logLevel)
-
-	if LogEnvironment == "production" {
-		log.SetFormatter(&log.JSONFormatter{})
-		roverHomeDir, _ := utils.GetRoverDirectory()
-		roverHomeLogsDir := filepath.Join(roverHomeDir, "logs")
-		command.EnsureDirectory(roverHomeLogsDir)
-		roverLogFile := filepath.Join(roverHomeLogsDir, "rover.log")
-		file, err := os.OpenFile(roverLogFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-		if err == nil {
-			log.SetOutput(file)
-		} else {
-			log.Info("Failed to log to file, using default stderr")
-		}
-	} else {
-		log.SetReportCaller(true) // has performance impact but useful in debugging
-	}
-
+	fileLog.SetLevel(logLevel)
+	fileLog.SetFormatter(&logrus.JSONFormatter{})
+	SetCommand("rover")
 }
 
 func SetLogLevel(level string, err error) {
 	lowerLevel := strings.ToLower(level)
 	switch lowerLevel {
 	case "trace":
-		logLevel = log.TraceLevel
+		logLevel = logrus.TraceLevel
 	case "debug":
-		logLevel = log.DebugLevel
+		logLevel = logrus.DebugLevel
 	case "info":
-		logLevel = log.InfoLevel
+		logLevel = logrus.InfoLevel
 	case "warn":
-		logLevel = log.WarnLevel
+		logLevel = logrus.WarnLevel
 	case "error":
-		logLevel = log.ErrorLevel
+		logLevel = logrus.ErrorLevel
 	case "fatal":
-		logLevel = log.FatalLevel
+		logLevel = logrus.FatalLevel
 	case "panic":
-		logLevel = log.PanicLevel
+		logLevel = logrus.PanicLevel
 	default:
-		logLevel = log.InfoLevel
+		logLevel = logrus.InfoLevel
+	}
+	fileLog.SetLevel(logLevel)
+}
+
+func SetCommand(rovercommand string) {
+
+	rovercommand = strings.ReplaceAll(rovercommand, " ", "")
+	if rovercommand == "" {
+		rovercommand = "rover"
+	}
+
+	roverHomeDir, _ := utils.GetRoverDirectory()
+	roverHomeLogsDir := filepath.Join(roverHomeDir, "logs")
+	command.EnsureDirectory(roverHomeLogsDir)
+
+	// Add datetime stamp to filename
+	currentTime := time.Now()
+	currentFile = rovercommand + "--" + currentTime.Format("2006-01-02--15-04") + ".log"
+
+	roverLogFile := filepath.Join(roverHomeLogsDir, currentFile)
+	file, err := os.OpenFile(roverLogFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err == nil {
+		fileLog.SetOutput(file)
+	} else {
+		stdOutLog.Info("Failed to log to file, using default stderr")
 	}
 }

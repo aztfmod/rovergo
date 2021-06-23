15543a290a36b5282036e0943b51b57e1510f062
PaulDFoster <paulfo@microsoft.com>
2021-06-23 18:06:09 +0100

    refactor

--- pkg/logger/entry.go
+++ pkg/logger/entry.go
@@ -5,15 +5,13 @@ import "github.com/sirupsen/logrus"
 type Entry Fields
 
 func (logFields *Entry) Error(args ...interface{}) {
-	f := logrus.Fields(*logFields)
-	fileEntry := fileLog.WithFields(f)
+	fileEntry := fileLog.WithFields(logrus.Fields(*logFields))
 	stdOutLog.WithFields(logrus.Fields{"For details see": currentFile}).Error(args...)
 	fileEntry.Error(args...)
 }
 
 func (logFields *Entry) Info(message string) {
-	f := logrus.Fields(*logFields)
-	fileEntry := fileLog.WithFields(f)
+	fileEntry := fileLog.WithFields(logrus.Fields(*logFields))
 	stdOutLog.Info(message)
 	fileEntry.Info(message)
 }

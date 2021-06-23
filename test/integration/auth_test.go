b5e91b113dcbfe3d455e2e7818313a93bfc7743e
greg oliver <golive@microsoft.com>
2021-06-23 07:45:13 +0000

    bugs

--- test/integration/auth_test.go
+++ test/integration/auth_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"encoding/json"
 	"testing"
 	"time"
 
@@ -44,6 +45,76 @@ func TestIntegration_VM_SystemAssigned_No_Role(t *testing.T) {
 
 	// should error out because no role assignment & no --allow-no-subscriptions for tenant level access
 	assert.Error(t, err)
+
+}
+
+func TestIntegration_VM_UserAssigned_SubOwner_Role(t *testing.T) {
+
+	defer clearup(t)
+
+	// use bootstrap identity to get the ball rolling
+	_, err := rovertesting.AzLoginBootstrap(t)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	id, err := rovertesting.AzIdentityCreate(t, "mi-user-assigned")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = rovertesting.AzRoleAssignmentCreate(t, id.PrincipalID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = rovertesting.AzVMIdentityAssign(t, id.PrincipalID, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// logout
+	err = rovertesting.AzLogout(t)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	loginSuccessful := false
+	for i := 0; i < 20; i++ {
+
+		// log in as the system assigned MI
+		_, err = rovertesting.AzLogin(t, "-i", "--username", id.ClientID)
+		if err == nil {
+			loginSuccessful = true
+			break
+		}
+
+		console.Warning("Waiting 15 seconds for next attempt")
+		time.Sleep(time.Second * 15)
+
+	}
+	if loginSuccessful == false {
+		t.Fatal("Failed to login as system assigned ID within 5 minutes")
+	}
+
+	// set up a terraform fmt command for the actual test
+	testCmd := &cobra.Command{
+		Use: "fmt",
+	}
+	testCmd.Flags().Bool("dry-run", true, "")
+	testCmd.Flags().String("config-dir", "../testdata/configs/level0/launchpad", "")
+	testCmd.Flags().String("source", "../testdata/caf-terraform-landingzones", "")
+	testCmd.Flags().String("level", "level0", "")
+	testCmd.Flags().Bool("launchpad", true, "")
+
+	optionsList := landingzone.BuildOptions(testCmd)
+
+	action := cmd.ActionMap[testCmd.Name()]
+	_ = action.Execute(&optionsList[0])
+
+	assert.Equal(t, "servicePrincipal", optionsList[0].Identity.ObjectType)
+	assert.Equal(t, "UserAssigned", optionsList[0].Identity.DisplayName)
+	assert.Equal(t, id.PrincipalID, optionsList[0].Identity.ObjectID)
 
 }
 
@@ -129,21 +200,58 @@ func clearup(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	ownerRoleAssignmentID, err := getOwnerRoleAssignmentID(t, vmIdentityDetails.PrincipalID)
-	if err != nil {
-		t.Fatal(err)
+	var assignmentIDs []string
+	if vmIdentityDetails.PrincipalID != "" {
+		roleAssignmentID, err := getOwnerRoleAssignmentID(t, vmIdentityDetails.PrincipalID)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assignmentIDs = append(assignmentIDs, roleAssignmentID)
 	}
 
-	if ownerRoleAssignmentID != "" {
-		err = rovertesting.AzRoleAssignmentDelete(t, ownerRoleAssignmentID)
+	for _, rawUserAssignedID := range vmIdentityDetails.UserAssignedIdentities {
+
+		userAssignedID := &rovertesting.UserAssignedIdentity{}
+		err = json.Unmarshal([]byte(rawUserAssignedID), userAssignedID)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		roleAssignmentID, err := getOwnerRoleAssignmentID(t, userAssignedID.PrincipalID)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assignmentIDs = append(assignmentIDs, roleAssignmentID)
+	}
+
+	for _, assignmentID := range assignmentIDs {
+		err = rovertesting.AzRoleAssignmentDelete(t, assignmentID)
 		if err != nil {
 			t.Fatal(err)
 		}
 	}
 
-	err = rovertesting.AzVMIdentityRemove(t, "[system]")
-	if err != nil {
-		t.Fatal(err)
+	if vmIdentityDetails.PrincipalID != "" {
+		err = rovertesting.AzVMIdentityRemove(t, "[system]")
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for _, rawUserAssignedID := range vmIdentityDetails.UserAssignedIdentities {
+
+		userAssignedID := &rovertesting.UserAssignedIdentity{}
+		err = json.Unmarshal([]byte(rawUserAssignedID), userAssignedID)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = rovertesting.AzVMIdentityRemove(t, userAssignedID.ID)
+		if err != nil {
+			t.Fatal(err)
+		}
+
 	}
 
 	err = rovertesting.AzLogout(t)

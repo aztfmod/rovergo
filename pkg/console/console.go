99a064c13a7a7409a5b326fa1d418798c54d1905
polatengin <enpolat@microsoft.com>
2021-08-18 20:35:01 +0300

    fixing color names in comment sections

--- pkg/console/console.go
+++ pkg/console/console.go
@@ -42,7 +42,7 @@ func Infof(f string, a ...interface{}) {
 	fmt.Printf("\033[1;34m"+f+"\033[0m", a...)
 }
 
-// Error outputs strings plus newline in blue
+// Error outputs strings plus newline in red
 func Error(s string) {
 	fmt.Printf("\033[1;31m%s\033[0m\n", s)
 }
@@ -52,7 +52,7 @@ func Errorf(f string, a ...interface{}) {
 	fmt.Printf("\033[1;31m"+f+"\033[0m", a...)
 }
 
-// Warning outputs strings plus newline in blue
+// Warning outputs strings plus newline in yellow
 func Warning(s string) {
 	fmt.Printf("\033[1;33m%s\033[0m\n", s)
 }
@@ -62,7 +62,7 @@ func Warningf(f string, a ...interface{}) {
 	fmt.Printf("\033[1;33m"+f+"\033[0m", a...)
 }
 
-// Success outputs strings plus newline in blue
+// Success outputs strings plus newline in green
 func Success(s string) {
 	fmt.Printf("\033[1;32m%s\033[0m\n", s)
 }

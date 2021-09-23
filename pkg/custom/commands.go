00eb8f1f437ea0afc30d67785d358288387b6734
polatengin <enpolat@microsoft.com>
2021-09-22 21:13:00 -0700

    creating initial go file to load, validate and execute custom commands and groups of commands

---
+++ pkg/custom/commands.go
@@ -0,0 +1,13 @@
+package custom
+
+import (
+	"errors"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/aztfmod/rover/pkg/builtin_actions"
+	"github.com/aztfmod/rover/pkg/landingzone"
+	"github.com/aztfmod/rover/pkg/rover"
+	"gopkg.in/yaml.v2"
+)

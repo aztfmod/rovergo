f609112848086ea786861e3a294b1e5236695cd2
polatengin <enpolat@microsoft.com>
2021-09-22 21:09:33 -0700

    builtin actions moved to separate file to fix cyclic dependency problem

---
+++ pkg/builtin_actions/builtin_actions.go
@@ -0,0 +1,13 @@
+package builtin_actions
+
+import "github.com/aztfmod/rover/pkg/landingzone"
+
+// ActionMap is exported so tests can use
+var ActionMap = map[string]landingzone.Action{
+	"init":     landingzone.NewInitAction(),
+	"plan":     landingzone.NewPlanAction(),
+	"apply":    landingzone.NewApplyAction(),
+	"destroy":  landingzone.NewDestroyAction(),
+	"validate": landingzone.NewValidateAction(),
+	"fmt":      landingzone.NewFormatAction(),
+}

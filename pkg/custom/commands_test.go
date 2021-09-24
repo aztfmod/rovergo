e4e46d3e1d1a0bf75d0031762fe4b9dc66fc119e
polatengin <enpolat@microsoft.com>
2021-09-23 20:57:54 -0700

    adding simple test to check the commands yaml doesnt exist condition

---
+++ pkg/custom/commands_test.go
@@ -0,0 +1,25 @@
+//
+// Rover - Unit tests for custom commands and group commands
+// * Engin Polat, September 2021
+//
+
+package custom
+
+import (
+	"testing"
+
+	"github.com/aztfmod/rover/pkg/console"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Commands_Yaml_Doesnt_Exists(t *testing.T) {
+
+	console.DebugEnabled = true
+
+	actions, err := LoadCustomCommandsAndGroups()
+
+	assert.Error(t, err)
+
+	assert.Empty(t, actions)
+
+}
